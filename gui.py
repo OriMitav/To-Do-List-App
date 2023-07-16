627a3c0872d213ddcf26d727da675fbf04d1016e
Ori Lenovo <grei12@gmail.com>
2023-07-16 21:19:26 +0300

    Start building the drop down list

--- gui.py
+++ gui.py
@@ -1,15 +1,18 @@
 import functions
 import PySimpleGUI as sg
 
-lable= sg.Text("Type in a to do")
-input_box=sg.InputText(tooltip="Enter to do", key="todo")
-add_button=sg.Button("Add")
-edit_button =sg.Button("Edit")
-list_box=sg.Listbox(values=)
+lable = sg.Text("Type in a to do")
+input_box = sg.InputText(tooltip="Enter to do", key="todo")
+add_button = sg.Button("Add")
+edit_button = sg.Button("Edit")
+list_box = sg.Listbox(values=functions.get_todos(),
+                      key='todos',
+                      enable_events=True,
+                      size=[45, 10])
 
-window=sg.Window('My to do app',
-                 layout=[[[lable],input_box,add_button,edit_button]],
-                 font=('Helvetica',20))
+window = sg.Window('My to do app',
+                   layout=[[lable], [input_box, add_button], [list_box, edit_button]],
+                   font=('Helvetica', 20))
 
 while True:
     event, values = window.read()
@@ -18,9 +21,16 @@ while True:
     match event:
         case "Add":
             todos = functions.get_todos()
-            new_todo = values['todo']+"\n"
+            new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.set_todos(todos)
+        case "Edit":
+            todo_to_edit = values['todos']
+            new_todo = values['todo']
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index]
+            """Keep writing the code from here"""
         case sg.WINDOW_CLOSED:
             break
 window.close()
