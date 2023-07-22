bdb93edf638ec8c943b704b0f7b7ab628c0aad4a
Ori Mitav <grei12@gmail.com>
2023-07-22 22:40:14 +0300

    Implement the complete method for the web versin

--- web.py
+++ web.py
@@ -4,7 +4,7 @@ import functions
 todos= functions.get_todos()
 
 def add_todo():
-    todo = st.session_state['new_todo'] + "\n"
+    todo = st.session_state['new_todo'] + "\n" # Creating dictionary contain given value into the key "new_todo"
     todos.append(todo)
     functions.set_todos(todos)
 
@@ -14,10 +14,18 @@ st.title("My todo App")
 st.subheader("This is my todo app.")
 st.write("This app will improve my productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.set_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label = "",
               placeholder = "Enter new todo...",
               on_change = add_todo,
               key='new_todo')
+
+st.session_state
+
