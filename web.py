0d069dc32b13b37c0d630f949522147cebb8842a
Ori Mitav <grei12@gmail.com>
2023-07-22 22:27:07 +0300

    Implement new todo to the web version

--- web.py
+++ web.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import functions
 
+todos= functions.get_todos()
+
+def add_todo():
+    todo = st.session_state['new_todo'] + "\n"
+    todos.append(todo)
+    functions.set_todos(todos)
+
 todos = functions.get_todos()
 
 st.title("My todo App")
@@ -10,5 +17,7 @@ st.write("This app will improve my productivity")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label = "", placeholder = "Enter new todo...")
-print("hy")
+st.text_input(label = "",
+              placeholder = "Enter new todo...",
+              on_change = add_todo,
+              key='new_todo')
